41827ccfabe9286155837ebc69ef8d5d36c1e778
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-01 16:59:04 +0530

    tested all the things and it is perfectly working

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StudentService service = new StudentService();
+        Student s1 = new Student(20,1,"Mubashir");
+        Student s2 = new Student(20,2,"Ahmed");
+        service.addStudent(s1);
+        service.addStudent(s2);
+        service.viewStudent();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
--- src/StudentService.java
+++ src/StudentService.java
@@ -2,9 +2,18 @@ import java.util.ArrayList;
 
 public class StudentService {
     ArrayList<Student> students = new ArrayList<>();
-public void addStudent(Student student){
-    students.add(student);
-    System.out.println("Student Added Successfully");
 
-}
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student Added Successfully");
+
+    }
+
+    public void viewStudent() {
+        for (Student student : students) {
+            System.out.println(
+                    student.getId() + " " + student.getAge() + " " + student.getName()
+            );
+        }
+    }
 }
