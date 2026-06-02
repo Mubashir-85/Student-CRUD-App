c13c8add7b861fe86e2e04027b46dbb82a3d951c
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-02 17:57:55 +0530

    Modified Methods

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         StudentService service = new StudentService();
 
--- src/StudentService.java
+++ src/StudentService.java
@@ -3,31 +3,37 @@ import java.util.ArrayList;
 public class StudentService {
     ArrayList<Student> students = new ArrayList<>();
 
-    public void addStudent(Student student) {
+    public void addStudent(Student student)throws InterruptedException {
         students.add(student);
+        Thread.sleep(2000);
         System.out.println("Student Added Successfully");
 
     }
 
-    public void viewStudent() {
+    public void viewStudent() throws InterruptedException {
         for (Student student : students) {
+            Thread.sleep(2000);
             System.out.println(
                     student.getId() + " " + student.getAge() + " " + student.getName()
             );
         }
     }
-    public void updateStudent(int id,  String name,int age){
+    public void updateStudent(int id,  String name,int age)throws InterruptedException{
         for(Student student: students){
             if(student.getId()== id){
                 student.setAge(age);
                 student.setName(name);
+                Thread.sleep(2000);
+                System.out.println("Student Updated Successfully");
+                return;
             }
         }
     }
-    public void deleteStudent(int id){
+    public void deleteStudent(int id)throws InterruptedException{
         for(Student student: students){
             if(student.getId()==id) {
                 students.remove(student);
+                Thread.sleep(2000);
                 System.out.println("Deleted Successfully");
                 return;
             }
@@ -35,9 +41,10 @@ public class StudentService {
         System.out.println("Student not found");
     }
 
-    public void searchStudent(int id){
+    public void searchStudent(int id)throws InterruptedException{
         for(Student student: students){
             if(student.getId()==id){
+                Thread.sleep(2000);
                 System.out.println("ID: " + student.getId());
                 System.out.println("Name: " + student.getName());
                 System.out.println("Age: " + student.getAge());
